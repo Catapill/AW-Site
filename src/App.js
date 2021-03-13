35e3027401d2568a7a70cbf2c1d395f09abf6134
Adam Woods <adam.woods13@icloud.com>
2021-03-13 18:54:15 +0000

    White screen fix three

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import './App.css';
 
 //COMPONENTS
